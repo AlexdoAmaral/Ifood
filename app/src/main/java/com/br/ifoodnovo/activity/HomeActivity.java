3892620f7843014386573dabeff6d5bfe4a6d419
Alex Amaral Ribeiro <alex.amaral@foursys.com.br>
2021-04-23 12:07:22 -0300

    Configurando as funcionalidades da Activity adapter_empresa

--- app/src/main/java/com/br/ifoodnovo/activity/HomeActivity.java
+++ app/src/main/java/com/br/ifoodnovo/activity/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.br.ifoodnovo.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,14 +13,29 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.br.ifoodnovo.R;
+import com.br.ifoodnovo.adapters.AdapterEmpresa;
 import com.br.ifoodnovo.helpers.ConfiguracaoFirebase;
+import com.br.ifoodnovo.model.Empresa;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 
 public class HomeActivity extends AppCompatActivity {
 
     private FirebaseAuth autenticacao;
     private MaterialSearchView searchView;
+    private RecyclerView recyclerEmpresa;
+    private List<Empresa> empresas = new ArrayList<>();
+    private DatabaseReference firebaseRef;
+    private AdapterEmpresa adapterEmpresa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +49,79 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Ifood");
         setSupportActionBar(toolbar);
+
+        //Configurando o Recycler View
+
+        recyclerEmpresa.setLayoutManager(new LinearLayoutManager(this));
+        recyclerEmpresa.setHasFixedSize(true);
+        adapterEmpresa = new AdapterEmpresa(empresas);
+        recyclerEmpresa.setAdapter(adapterEmpresa);
+
+        //Recuperando Empresas
+        recuperarEmpresas();
+
+        //Configurando o SearchView
+        searchView.setHint("Pesqauisar Restaurantes...");
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                pesquisarEmpresas(newText);
+                return true;
+            }
+        });
     }
 
+    private void pesquisarEmpresas(String minhaPesquisa) {
+        //Criando Ref
+        DatabaseReference empresaRef = firebaseRef.child("empresas");
+        //Criando a Query
+        Query query = empresaRef.orderByChild("nome").startAt(minhaPesquisa + "\uf8ff");
+        //Configurando a query
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+                empresas.clear();
+                for (DataSnapshot ds : datasnapshot.getChildren()) {
+                    empresas.add(ds.getValue(Empresa.class));
+                }
+                adapterEmpresa.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void recuperarEmpresas() {
+        DatabaseReference empresaRef = firebaseRef.child("empresas");
+        empresaRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+                empresas.clear();
+                for (DataSnapshot ds : datasnapshot.getChildren()) {
+                    empresas.add(ds.getValue(Empresa.class));
+                }
+                adapterEmpresa.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+
     // Criando as opções do menu
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_usuario, menu);
 
@@ -46,12 +133,12 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     // Verificando a opção selecionada pelo usuário
-    public boolean onOptionsItemSelected(MenuItem item){
-        switch (item.getItemId()){
-            case R.id.menuSair :
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuSair:
                 deslogarUsuario();
                 break;
-            case R.id.menuConfiguracoes :
+            case R.id.menuConfiguracoes:
                 abrirConfiguracoes();
                 break;
         }
@@ -68,12 +155,12 @@ public class HomeActivity extends AppCompatActivity {
         }
     }
 
-    private void abrirConfiguracoes(){
+    private void abrirConfiguracoes() {
         startActivity(new Intent(HomeActivity.this, ConfiguracaoUsuarioActivity.class));
     }
 
 
-    private void inicializaComponentes(){
+    private void inicializaComponentes() {
         searchView = findViewById(R.id.materialSearchView);
     }
 }
